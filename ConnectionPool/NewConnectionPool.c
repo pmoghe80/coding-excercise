fbccc76a4def0767f4e34348bbcd783db271fe19
root <root@linux-tiig.site>
2018-03-23 00:20:16 +0530

    Connection Pool function definition

---
+++ ConnectionPool/NewConnectionPool.c
@@ -0,0 +1,33 @@
+#include "NewConnectionPool.h"
+
+ConnPool_t initConnPool () {
+
+    ConnPool_t conn_pool = (ConnPool_t)malloc(sizeof(*conn_pool));
+    if (!conn_pool) {
+       printf("Conn Pool malloc error\n");
+       return NULL;
+    }
+
+   conn_pool->pool = (RemoteConn_t)malloc(sizeof(RemoteConn_t)*INIT_POOL_SIZE);
+   if (!conn_pool->pool) {
+      printf("Remote Conn Pool malloc error\n");
+      return NULL;
+   }
+ 
+   conn_pool->num_conn = 0;
+
+   pthread_mutex_init(&conn_pool->mux, NULL);
+   pthread_cond_init(&conn_pool->cond, NULL);
+
+   return conn_pool;
+}  
+    
+
+    
+  
+
+int main() {
+
+  printf("Hello World\n");
+  return 0;
+}
