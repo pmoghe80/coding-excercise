e3b6a34b3a62ea416dc5bf6d1331f641aaee6f74
root <root@linux-tiig.site>
2018-03-24 17:07:09 +0530

    getConnection function complete

--- ConnectionPool/NewConnectionPool.c
+++ ConnectionPool/NewConnectionPool.c
@@ -8,7 +8,7 @@ ConnPool_t initConnPool () {
        return NULL;
     }
 
-   conn_pool->pool = (RemoteConn_t)malloc(sizeof(RemoteConn_t)*INIT_POOL_SIZE);
+   conn_pool->pool = (RemoteConn_t)malloc(sizeof(*(conn_pool->pool))*INIT_POOL_SIZE);
    if (!conn_pool->pool) {
       printf("Remote Conn Pool malloc error\n");
       return NULL;
@@ -22,15 +22,51 @@ ConnPool_t initConnPool () {
    return conn_pool;
 }  
     
-int getConnection (ConnPool_t conn_pool) {
+RemoteConn_t getConnection (ConnPool_t conn_pool, int ip, int port) {
 
     if (!conn_pool) {
-        printf("%s: Invalid Connection Pool", __FUNC__);
-        return 0;
+        printf("%s: Invalid Connection Pool", __func__);
+        return NULL;
     }
     
+    pthread_mutex_lock(&conn_pool->mux);
+    
     for (int i=0; i<conn_pool->num_conn; i++) {
-        if (conn
+        assert(conn_pool->pool+i);
+        if (conn_pool->pool[i]->ip == ip && 
+            conn_pool->pool[i]->port == port) {
+             RemoteConn_t rem_conn = conn_pool->pool[i];
+             pthread_mutex_unlock(&conn_pool->mux);
+             pthread_mutex_lock(&rem_conn->mux);
+             while (rem_conn->in_use) {
+                 pthread_cond_wait(&rem_conn->cond);
+             }
+             rem_conn->in_use = 1;
+             pthread_mutex_unlock(&rem_conn->mux);
+             return rem_conn; 
+        }
+    }
+
+    if (conn_pool->num_conn < MAX_POOL_SIZE) {
+        printf("%s: Connection not found in pool, allocating\n", __func__);
+        RemoteConn_t rem_conn = (RemoteConn_t)malloc(sizeof(*rem_conn));
+        if (!rem_conn) {
+           printf("%s: Remote connection memory allocation failed\n", __func__);
+           pthread_mutex_unlock(&conn_pool->mux);
+           return NULL;
+        }
+        initRemoteConnection(rem_conn);
+        conn_pool->pool[num_conn++] = rem_conn;
+        pthread_mutex_unlock(&conn_pool->mux);
+        return rem_conn;
+    } else {
+       reallocConnectionPool(&conn_pool);
+       pthread_mutex_unlock(&conn_pool->mux);
+       getConnection(conn_pool, ip, port);
+       pthread_mutex_lock(&conn_pool->mux);
+    }
+    return NULL;
+}        
     
   
 
