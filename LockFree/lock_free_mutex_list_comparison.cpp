80daeba9c2f1b1b038d24e9447112c4f3d45df69
Piyush Moghe <pmoghe@gmail.com>
2019-06-25 07:51:36 +0000

    Measured time using by running with time command and atomic wins

--- LockFree/lock_free_mutex_list_comparison.cpp
+++ LockFree/lock_free_mutex_list_comparison.cpp
@@ -19,90 +19,80 @@ Node_t           mhead;
 pthread_mutex_t  mlock;
 void * AtomicAddNode (void *arg) {
      int data = *(int *)arg;
-     //for (int i=0; i < 5000; i++) {
      Node_t tmp = (Node_t)malloc(sizeof(*tmp));
 
      tmp->data = data; 
      tmp->next = ahead;
 
-     //srand(1);
-     //printf("Adding Node %d\n", data);
      while (!atomic_compare_exchange_weak(&ahead, &tmp->next, tmp)) {
           
      }
-      // data++; 
-     //}
-     //sleep(rand()%30000);
 }
 
 void * MutexAddNode (void *arg) {
      int data = *(int *)arg;
-     //for (int i=0; i < 5000; i++) {
      Node_t tmp = (Node_t)malloc(sizeof(*tmp));
 
      tmp->data = data; 
      tmp->next = ahead;
 
-     //srand(1);
-     //printf("Mutex Adding Node %d\n", data);
      pthread_mutex_lock(&mlock);
      tmp->next = mhead;
      mhead = tmp;
      pthread_mutex_unlock(&mlock);
-     //sleep(rand()%30000);  
-      //data++;
-    // }
      return NULL;
 }
 
 
 int main() {
-    pthread_t apt[30000];
-    pthread_t mpt[30000];
+    pthread_t apt[10000];
+    pthread_t mpt[10000];
     int i=0;
-    int arr[30000];
+    int arr[10000];
+    struct timeval start_tv;
+    struct timeval end_tv;
     struct timeval tv;
     long long int start, end;
     long long int start_usec, end_usec;
 
-    //arr[0] = 1;
-    //arr[1] = 5000;
-    gettimeofday(&tv, NULL);
-    start = tv.tv_sec;
-    start_usec = tv.tv_usec; 
-    while (i < 30000) {
+    //gettimeofday(&start_tv, NULL);
+    //start = tv.tv_sec;
+    //start_usec = tv.tv_usec; 
+    while (i < 10000) {
          arr[i] = i;
          pthread_create(&apt[i], NULL, AtomicAddNode, (void *)&arr[i]);
          i++;
     }       
      
-    for (i=0; i<30000; i++) {
+    for (i=0; i<10000; i++) {
          pthread_join(apt[i], NULL);
-    }  
+    } 
 
-    gettimeofday(&tv, NULL); 
-    end = tv.tv_sec;
-    end_usec = tv.tv_usec; 
- 
-    printf("Atomic Time taken: %lld, %lld, %lld\n", end-start, start_usec , end_usec);
+    //gettimeofday(&end_tv, NULL); 
+    //end = tv.tv_sec;
+    //end_usec = tv.tv_usec; 
+    //timersub(&start_tv, &end_tv, &tv); 
+    //printf("Atomic Time taken: %ld, %ld\n", (long int)tv.tv_sec, (long int)tv.tv_usec);
 
     /******* MUTEX Code Starts ***********/
-    gettimeofday(&tv, NULL);
-    start = tv.tv_sec;
+    //gettimeofday(&start_tv, NULL);
+    //start = tv.tv_sec;
     pthread_mutex_init(&mlock, NULL);
     i = 0;
-    while (i < 30000) {
+    while (i < 10000) {
          arr[i] = i;
          pthread_create(&mpt[i], NULL, MutexAddNode, (void *)&arr[i]);
          i++;
     }       
 
-    for (i=0; i<30000; i++) {
+    for (i=0; i<10000; i++) {
          pthread_join(mpt[i], NULL);
-    }  
-    gettimeofday(&tv, NULL); 
-    end = tv.tv_sec;
-    printf("Mutex Time taken: %lld, %lld, %lld\n", end-start, start_usec, end_usec);
+    } 
+    //gettimeofday(&end_tv, NULL); 
+    //end = tv.tv_sec;
+    //printf("Mutex Time taken: %lld, %lld, %lld\n", end-start, start_usec, end_usec);
+    //timersub(&start_tv, &end_tv, &tv); 
+    //printf("Mutex Time taken: %ld, %ld\n", (long int)tv.tv_sec, (long int)tv.tv_usec);
 
     return 0; 
 }
